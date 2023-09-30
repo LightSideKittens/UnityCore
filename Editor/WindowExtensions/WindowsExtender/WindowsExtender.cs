978f24cc56b10b3905ba62c413cfae8cbb5a239e
Malvis Light <malvislight@gmail.com>
2023-10-01 00:17:19 +0400

    Added Harmony Library and Fix ApiCompabilityLevel❤️

--- Editor/WindowExtensions/WindowsExtender/WindowsExtender.cs
+++ Editor/WindowExtensions/WindowsExtender/WindowsExtender.cs
@@ -1,28 +1,29 @@
 using System;
 using System.Collections.Generic;
 using System.Reflection;
-//using HarmonyLib;
-using Sirenix.Utilities;
+using HarmonyLib;
 using UnityEditor;
-using UnityEngine;
 
 [InitializeOnLoad]
 internal static class WindowsExtender
 {
-    //public static Harmony Harmony { get; }
+    public static Harmony Harmony { get; }
     public static MethodInfo OnPreGUIMethod { get; }
     public static MethodInfo OnPostGUIMethod { get; }
     public static Dictionary<Type, BaseWindowExtender> Extenders { get; } = new();
     
     static WindowsExtender()
     {
+        var currentGroup = EditorUserBuildSettings.selectedBuildTargetGroup;
+        PlayerSettings.SetApiCompatibilityLevel(currentGroup, ApiCompatibilityLevel.NET_Unity_4_8);
+        AssetDatabase.Refresh();
         const BindingFlags flags = BindingFlags.Static | BindingFlags.Public;
         OnPreGUIMethod = typeof(WindowsExtender).GetMethod(nameof(OnPreGUI), flags);
         OnPostGUIMethod = typeof(WindowsExtender).GetMethod(nameof(OnPostGUI), flags);
         
-        var assembly = Assembly.Load("LGCore.Editor.WindowExtensions");
+        var assembly = Assembly.Load("LSCore.Editor.WindowExtensions");
         var types = assembly.GetTypes();
-        //Harmony = new Harmony("com.yourname.yourproject");
+        Harmony = new Harmony("com.yourname.yourproject");
         var baseType = typeof(BaseWindowExtender);
         
         for (int i = 0; i < types.Length; i++)
