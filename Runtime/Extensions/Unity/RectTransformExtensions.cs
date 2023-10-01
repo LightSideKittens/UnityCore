81f0dfea3a044d30430c0f5456edd6ef44f36a16
Malvis Light <malvislight@gmail.com>
2023-10-02 02:05:32 +0400

    Added some RectTransform Extensions❤️

---
+++ Runtime/Extensions/Unity/RectTransformExtensions.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace LSCore.Extensions.Unity
+{
+    public static class RectTransformExtensions
+    {
+        public static RectTransform Parent(this RectTransform target) => target.parent.GetComponent<RectTransform>();
+
+        public static Vector2 GetLocalPositionByScreenPoint(this RectTransform parent, Vector2 screenPoint, Canvas canvas = null)
+        {
+            Camera cam = null;
+            
+            if (canvas)
+            {
+                cam = canvas.renderMode == RenderMode.ScreenSpaceOverlay ? null : canvas.worldCamera;
+            }
+            
+            
+            RectTransformUtility.ScreenPointToLocalPointInRectangle(parent, screenPoint, cam, out var localPoint);
+            return localPoint;
+        }
+        
+        public static void SetPositionByScreenPoint(this RectTransform target, RectTransform parent, Vector2 screenPoint, Canvas canvas = null)
+        {
+            target.localPosition = parent.GetLocalPositionByScreenPoint(screenPoint, canvas);
+        }
+    }
+}
