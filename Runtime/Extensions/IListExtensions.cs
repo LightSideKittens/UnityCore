62a33cdae497a3a34d3ff1bcd2e0416d68576076
Malvis Light <malvislight@gmail.com>
2023-10-03 04:41:26 +0400

    Implemented Binary Search❤️

--- Runtime/Extensions/IListExtensions.cs
+++ Runtime/Extensions/IListExtensions.cs
@@ -1,9 +1,46 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace LSCore.Extensions
 {
     public static class IListExtensions
     {
         public static T Random<T>(this IList<T> list) => list[UnityEngine.Random.Range(0, list.Count)];
+
+        public static int ClosestBinarySearch(Func<int, int> arr, int length, int target)
+        {
+            int left = 0;
+            int right = length - 1;
+
+            if (length == 2)
+            {
+                return target > arr(0) ? 1 : 0;
+            }
+
+            while (left < right - 1) // Keep looping until left and right are adjacent
+            {
+                int mid = left + (right - left) / 2;
+
+                if (arr(mid) == target)
+                {
+                    return mid; // Found the exact target
+                }
+                else if (arr(mid) < target)
+                {
+                    left = mid; // Move left pointer to mid
+                }
+                else
+                {
+                    right = mid; // Move right pointer to mid
+                }
+            }
+
+            if (target > arr(left))
+            {
+                return right;
+            }
+
+            return left;
+        }
     }
 }
