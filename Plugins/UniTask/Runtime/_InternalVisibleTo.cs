5845891ee2589cf8f9075542646b78b778529301
Malvis Light <malvislight@gmail.com>
2023-10-29 17:12:18 +0400

    Revert "Revert "Removed unused UniTask❤️""
    This reverts commit 961660aee2cc943bd7f4bbdc77258f35efca0b2b.

--- Plugins/UniTask/Runtime/_InternalVisibleTo.cs
+++
@@ -1,6 +0,0 @@
-﻿using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("LSCore.UniTask.Linq")]
-[assembly: InternalsVisibleTo("LSCore.UniTask.Addressables")]
-[assembly: InternalsVisibleTo("LSCore.UniTask.DOTween")]
-[assembly: InternalsVisibleTo("LSCore.UniTask.TextMeshPro")]
