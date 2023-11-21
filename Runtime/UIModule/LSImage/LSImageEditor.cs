d141a89f9fa0ccb764e9b3556035fe4b716434a6
Malvis Light <malvislight@gmail.com>
2023-11-22 02:45:17 +0400

    LSImage. Remove useless handles ❤️

--- Runtime/UIModule/LSImage/LSImageEditor.cs
+++ Runtime/UIModule/LSImage/LSImageEditor.cs
@@ -63,10 +63,9 @@ namespace LSCore
             
             if (image.Gradient.Count > 1)
             {
-                if (GUILayout.Button(isEditing ? "Stop Edit" : "Edit", GUILayout.Height(30)))
-                {
-                    isEditing = !isEditing;
-                }
+                angle.floatValue = EditorGUILayout.Slider("Angle", angle.floatValue, 0, 360);
+                gradientStart.floatValue = EditorGUILayout.Slider("Gradient Start", gradientStart.floatValue, -1, 1);
+                gradientEnd.floatValue = EditorGUILayout.Slider("Gradient End", gradientEnd.floatValue, -1, 1);
             }
             else
             {
@@ -122,112 +121,6 @@ namespace LSCore
             }
 
             GUILayout.EndHorizontal();
-        }
-
-        private Dictionary<int, int> map = new();
-
-        void OnSceneGUI()
-        {
-            if (!isEditing) return;
-
-            var imageTransform = image.transform;
-            var imagePosition = imageTransform.position;
-            var imageRotation = imageTransform.rotation.eulerAngles.z;
-
-            Vector3 center = (Vector3)image.rectTransform.rect.center + imagePosition;
-            var center2D = (Vector2)center;
-
-            var start = (image.gradientStartPoint + center2D).RotateAroundPoint(imagePosition, imageRotation);
-            var end = (image.gradientEndPoint + center2D).RotateAroundPoint(imagePosition, imageRotation);
-
-            float handle1Size = HandleUtility.GetHandleSize(start) * 0.1f;
-            float handle2Size = HandleUtility.GetHandleSize(end) * 0.1f;
-            Handles.DrawSolidDisc(start, Vector3.forward, handle1Size);
-            Handles.DrawSolidDisc(end, Vector3.forward, handle2Size);
-            var id1 = GUIUtility.GetControlID(FocusType.Passive);
-            var id2 = GUIUtility.GetControlID(FocusType.Passive);
-            if (id1 == -1) return;
-            map.Clear();
-            map.Add(id1, 0);
-            map.Add(id2, 1);
-            HandleInput(id1);
-            HandleInput(id2);
-            EditorGUI.BeginChangeCheck();
-            Vector3 newHandle1Pos = Handles.FreeMoveHandle(id1, start, handle1Size * 1.5f, Vector3.zero,
-                Handles.CircleHandleCap);
-            Vector3 newHandle2Pos =
-                Handles.FreeMoveHandle(id2, end, handle2Size * 1.5f, Vector3.zero, Handles.CircleHandleCap);
-            
-            if (EditorGUI.EndChangeCheck())
-            {
-                newHandle1Pos = ((Vector2)newHandle1Pos).RotateAroundPoint(imagePosition, -imageRotation);
-                newHandle2Pos = ((Vector2)newHandle2Pos).RotateAroundPoint(imagePosition, -imageRotation);
-                newHandle1Pos -= center;
-                newHandle2Pos -= center;
-                
-                image.GradientStart = newHandle1Pos.UnclampedInverseLerp(image.minPoint, image.maxPoint);
-                image.GradientEnd = newHandle2Pos.UnclampedInverseLerp(image.maxPoint, image.minPoint);
-                Undo.RecordObject(image, "Move Handle");
-                EditorUtility.SetDirty(image);
-            }
-
-            EditorGUI.BeginChangeCheck();
-            center = center2D.RotateAroundPoint(imagePosition, imageRotation);
-            var newAngle = Handles.RotationHandle(Quaternion.Euler(Vector3.forward * (image.Angle + imageRotation)),
-                center);
-            if (EditorGUI.EndChangeCheck())
-            {
-                image.Angle = newAngle.eulerAngles.z - imageRotation;
-                EditorUtility.SetDirty(image);
-            }
-        }
-
-        private static readonly float DoubleClickTime = 0.3f; // Time threshold for double click
-        private float lastClickTime = 0f;
-        private int currentControlId;
-
-        private void HandleInput(int controlId)
-        {
-            Event e = Event.current;
-            var type = e.GetTypeForControl(controlId);
-
-            switch (type)
-            {
-                case EventType.MouseDown:
-                    if (HandleUtility.nearestControl == controlId && e.button == 0) // Left mouse button
-                    {
-                        float timeSinceLastClick = (float)(EditorApplication.timeSinceStartup - lastClickTime);
-
-                        if (timeSinceLastClick < DoubleClickTime)
-                        {
-                            OpenColorPicker(map[controlId]);
-                        }
-
-                        lastClickTime = (float)EditorApplication.timeSinceStartup;
-                    }
-
-                    break;
-                case EventType.ExecuteCommand:
-                    if (e.commandName == "ColorPickerChanged")
-                    {
-                        /*var colors = image.Gradient.colorKeys;
-                        var alphaKeys = image.Gradient.alphaKeys;
-                        colors[currentControlId].color = LSColorPicker.Color;
-                        alphaKeys[currentControlId].alpha = LSColorPicker.Color.a;
-                        image.Gradient.SetKeys(colors, alphaKeys);
-                        image.SetVerticesDirty();*/
-                    }
-
-                    break;
-            }
-        }
-
-        private void OpenColorPicker(int cotrolID)
-        {
-            currentControlId = cotrolID;
-            var color = image.Gradient[cotrolID].color;
-
-            LSColorPicker.Show(newColor => { }, color);
         }
 
         [MenuItem("GameObject/LSCore/Image")]
