fd2bcffc656439f4b3763bb43afb2c6f49b66a4b
Malvis Light <cat@lightside.games>
2024-09-22 14:52:53 +0400

    Refactored Fixed UIParticleSystem❤️

--- Runtime/CommonComponents/UIParticleSystem.cs
+++ Runtime/CommonComponents/UIParticleSystem.cs
@@ -1,8 +1,7 @@
-﻿/// Credit glennpow, Zarlang
-/// Sourced from - http://forum.unity3d.com/threads/free-script-particle-systems-in-ui-screen-space-overlay.406862/
-/// Updated by Zarlang with a more robust implementation, including TextureSheet annimation support
+﻿using UnityEngine;
+using UnityEngine.UI;
 
-namespace UnityEngine.UI.Extensions.CasualGame
+namespace LSCore
 {
 #if UNITY_5_3_OR_NEWER
     [ExecuteInEditMode]
@@ -14,65 +13,53 @@ namespace UnityEngine.UI.Extensions.CasualGame
         public bool fixedTime = true;
 
         [Tooltip("Enables 3d rotation for the particles")]
-        public bool use3dRotation = false;
+        public bool use3dRotation;
 
-        private Transform _transform;
-        private ParticleSystem pSystem;
+        private readonly UIVertex[] quad = new UIVertex[4];
+        private new Transform transform;
+        private ParticleSystem ps;
         private ParticleSystem.Particle[] particles;
-        private UIVertex[] _quad = new UIVertex[4];
         private Vector4 imageUV = Vector4.zero;
         private ParticleSystem.TextureSheetAnimationModule textureSheetAnimation;
         private int textureSheetAnimationFrames;
         private Vector2 textureSheetAnimationFrameSize;
         private ParticleSystemRenderer pRenderer;
-        private bool isInitialised = false;
+        private bool isInitialised;
 
         private Material currentMaterial;
 
         private Texture currentTexture;
 
-#if UNITY_5_5_OR_NEWER
         private ParticleSystem.MainModule mainModule;
-#endif
 
-        public override Texture mainTexture
+        public override Texture mainTexture => currentTexture;
+
+        private bool Initialize()
         {
-            get
+            if (transform == null)
             {
-                return currentTexture;
+                transform = base.transform;
             }
-        }
-
-        protected bool Initialize()
-        {
-            // initialize members
-            if (_transform == null)
+            if (ps == null)
             {
-                _transform = transform;
-            }
-            if (pSystem == null)
-            {
-                pSystem = GetComponent<ParticleSystem>();
+                ps = GetComponent<ParticleSystem>();
 
-                if (pSystem == null)
+                if (ps == null)
                 {
                     return false;
                 }
 
-#if UNITY_5_5_OR_NEWER
-                mainModule = pSystem.main;
-                if (pSystem.main.maxParticles > 14000)
+                mainModule = ps.main;
+                if (ps.main.maxParticles > 14000)
                 {
                     mainModule.maxParticles = 14000;
                 }
-#else
-                    if (pSystem.maxParticles > 14000)
-                        pSystem.maxParticles = 14000;
-#endif
 
-                pRenderer = pSystem.GetComponent<ParticleSystemRenderer>();
+                pRenderer = ps.GetComponent<ParticleSystemRenderer>();
                 if (pRenderer != null)
+                {
                     pRenderer.enabled = false;
+                }
 
                 if (material == null)
                 {
@@ -88,30 +75,21 @@ namespace UnityEngine.UI.Extensions.CasualGame
                 {
                     currentTexture = currentMaterial.mainTexture;
                     if (currentTexture == null)
+                    {
                         currentTexture = Texture2D.whiteTexture;
+                    }
                 }
                 material = currentMaterial;
-                // automatically set scaling
-#if UNITY_5_5_OR_NEWER
                 mainModule.scalingMode = ParticleSystemScalingMode.Hierarchy;
-#else
-                    pSystem.scalingMode = ParticleSystemScalingMode.Hierarchy;
-#endif
 
                 particles = null;
             }
-#if UNITY_5_5_OR_NEWER
-            if (particles == null)
-                particles = new ParticleSystem.Particle[pSystem.main.maxParticles];
-#else
-                if (particles == null)
-                    particles = new ParticleSystem.Particle[pSystem.maxParticles];
-#endif
+            
+            particles ??= new ParticleSystem.Particle[ps.main.maxParticles];
 
             imageUV = new Vector4(0, 0, 1, 1);
 
-            // prepare texture sheet animation
-            textureSheetAnimation = pSystem.textureSheetAnimation;
+            textureSheetAnimation = ps.textureSheetAnimation;
             textureSheetAnimationFrames = 0;
             textureSheetAnimationFrameSize = Vector2.zero;
             if (textureSheetAnimation.enabled)
@@ -127,7 +105,9 @@ namespace UnityEngine.UI.Extensions.CasualGame
         {
             base.Awake();
             if (!Initialize())
+            {
                 enabled = false;
+            }
         }
 
 
@@ -142,7 +122,6 @@ namespace UnityEngine.UI.Extensions.CasualGame
                 }
             }
 #endif
-            // prepare vertices
             vh.Clear();
 
             if (!gameObject.activeInHierarchy)
@@ -150,48 +129,36 @@ namespace UnityEngine.UI.Extensions.CasualGame
                 return;
             }
 
-            if (!isInitialised && !pSystem.main.playOnAwake)
+            if (!isInitialised && !ps.main.playOnAwake)
             {
-                pSystem.Stop(false, ParticleSystemStopBehavior.StopEmittingAndClear);
+                ps.Stop(false, ParticleSystemStopBehavior.StopEmittingAndClear);
                 isInitialised = true;
             }
 
-            Vector2 temp = Vector2.zero;
-            Vector2 corner1 = Vector2.zero;
-            Vector2 corner2 = Vector2.zero;
-            // iterate through current particles
-            int count = pSystem.GetParticles(particles);
+            var temp = Vector2.zero;
+            var corner1 = Vector2.zero;
+            var corner2 = Vector2.zero;
+            var count = ps.GetParticles(particles);
 
-            for (int i = 0; i < count; ++i)
+            for (var i = 0; i < count; ++i)
             {
-                ParticleSystem.Particle particle = particles[i];
+                var particle = particles[i];
 
-                // get particle properties
-#if UNITY_5_5_OR_NEWER
-                Vector2 position = (mainModule.simulationSpace == ParticleSystemSimulationSpace.Local ? particle.position : _transform.InverseTransformPoint(particle.position));
-#else
-                    Vector2 position = (pSystem.simulationSpace == ParticleSystemSimulationSpace.Local ? particle.position : _transform.InverseTransformPoint(particle.position));
-#endif
-                float rotation = -particle.rotation * Mathf.Deg2Rad;
-                float rotation90 = rotation + Mathf.PI / 2;
-                Color32 color = particle.GetCurrentColor(pSystem);
-                float size = particle.GetCurrentSize(pSystem) * 0.5f;
-
-                // apply scale
-#if UNITY_5_5_OR_NEWER
+                Vector2 position = (mainModule.simulationSpace == ParticleSystemSimulationSpace.Local ? particle.position : transform.InverseTransformPoint(particle.position));
+                var rotation = -particle.rotation * Mathf.Deg2Rad;
+                var rotation90 = rotation + Mathf.PI / 2;
+                var color = particle.GetCurrentColor(ps);
+                var size = particle.GetCurrentSize(ps) * 0.5f;
+                
                 if (mainModule.scalingMode == ParticleSystemScalingMode.Shape)
+                {
                     position /= canvas.scaleFactor;
-#else
-                    if (pSystem.scalingMode == ParticleSystemScalingMode.Shape)
-                        position /= canvas.scaleFactor;
-#endif
+                }
 
-                // apply texture sheet animation
-                Vector4 particleUV = imageUV;
+                var particleUV = imageUV;
                 if (textureSheetAnimation.enabled)
                 {
-#if UNITY_5_5_OR_NEWER
-                    float frameProgress = 1 - (particle.remainingLifetime / particle.startLifetime);
+                    var frameProgress = 1 - (particle.remainingLifetime / particle.startLifetime);
 
                     if (textureSheetAnimation.frameOverTime.curveMin != null)
                     {
@@ -205,12 +172,9 @@ namespace UnityEngine.UI.Extensions.CasualGame
                     {
                         frameProgress = textureSheetAnimation.frameOverTime.constant - (particle.remainingLifetime / particle.startLifetime);
                     }
-#else
-                    float frameProgress = 1 - (particle.lifetime / particle.startLifetime);
-#endif
 
                     frameProgress = Mathf.Repeat(frameProgress * textureSheetAnimation.cycleCount, 1);
-                    int frame = 0;
+                    var frame = 0;
 
                     switch (textureSheetAnimation.animation)
                     {
@@ -222,10 +186,7 @@ namespace UnityEngine.UI.Extensions.CasualGame
                         case ParticleSystemAnimationType.SingleRow:
                             frame = Mathf.FloorToInt(frameProgress * textureSheetAnimation.numTilesX);
 
-                            int row = textureSheetAnimation.rowIndex;
-                            //                    if (textureSheetAnimation.useRandomRow) { // FIXME - is this handled internally by rowIndex?
-                            //                        row = Random.Range(0, textureSheetAnimation.numTilesY, using: particle.randomSeed);
-                            //                    }
+                            var row = textureSheetAnimation.rowIndex;
                             frame += row * textureSheetAnimation.numTilesX;
                             break;
 
@@ -242,31 +203,30 @@ namespace UnityEngine.UI.Extensions.CasualGame
                 temp.x = particleUV.x;
                 temp.y = particleUV.y;
 
-                _quad[0] = UIVertex.simpleVert;
-                _quad[0].color = color;
-                _quad[0].uv0 = temp;
+                quad[0] = UIVertex.simpleVert;
+                quad[0].color = color;
+                quad[0].uv0 = temp;
 
                 temp.x = particleUV.x;
                 temp.y = particleUV.w;
-                _quad[1] = UIVertex.simpleVert;
-                _quad[1].color = color;
-                _quad[1].uv0 = temp;
+                quad[1] = UIVertex.simpleVert;
+                quad[1].color = color;
+                quad[1].uv0 = temp;
 
                 temp.x = particleUV.z;
                 temp.y = particleUV.w;
-                _quad[2] = UIVertex.simpleVert;
-                _quad[2].color = color;
-                _quad[2].uv0 = temp;
+                quad[2] = UIVertex.simpleVert;
+                quad[2].color = color;
+                quad[2].uv0 = temp;
 
                 temp.x = particleUV.z;
                 temp.y = particleUV.y;
-                _quad[3] = UIVertex.simpleVert;
-                _quad[3].color = color;
-                _quad[3].uv0 = temp;
+                quad[3] = UIVertex.simpleVert;
+                quad[3].color = color;
+                quad[3].uv0 = temp;
 
                 if (rotation == 0)
                 {
-                    // no rotation
                     corner1.x = position.x - size;
                     corner1.y = position.y - size;
                     corner2.x = position.x + size;
@@ -274,66 +234,51 @@ namespace UnityEngine.UI.Extensions.CasualGame
 
                     temp.x = corner1.x;
                     temp.y = corner1.y;
-                    _quad[0].position = temp;
+                    quad[0].position = temp;
                     temp.x = corner1.x;
                     temp.y = corner2.y;
-                    _quad[1].position = temp;
+                    quad[1].position = temp;
                     temp.x = corner2.x;
                     temp.y = corner2.y;
-                    _quad[2].position = temp;
+                    quad[2].position = temp;
                     temp.x = corner2.x;
                     temp.y = corner1.y;
-                    _quad[3].position = temp;
+                    quad[3].position = temp;
                 }
                 else
                 {
                     if (use3dRotation)
                     {
-                        // get particle properties
-#if UNITY_5_5_OR_NEWER
-                        Vector3 pos3d = (mainModule.simulationSpace == ParticleSystemSimulationSpace.Local ? particle.position : _transform.InverseTransformPoint(particle.position));
-#else
-                        Vector3 pos3d = (pSystem.simulationSpace == ParticleSystemSimulationSpace.Local ? particle.position : _transform.InverseTransformPoint(particle.position));
-#endif
+                        var pos3d = (mainModule.simulationSpace == ParticleSystemSimulationSpace.Local ? particle.position : transform.InverseTransformPoint(particle.position));
 
-                        // apply scale
-#if UNITY_5_5_OR_NEWER
-                        if (mainModule.scalingMode == ParticleSystemScalingMode.Shape)
-                            position /= canvas.scaleFactor;
-#else
-                        if (pSystem.scalingMode == ParticleSystemScalingMode.Shape)
-                            position /= canvas.scaleFactor;
-#endif
-
-                        Vector3[] verts = new Vector3[4]
+                        var verts = new Vector3[]
                         {
-                            new Vector3(-size, -size, 0),
-                            new Vector3(-size, size, 0),
-                            new Vector3(size, size, 0),
-                            new Vector3(size, -size, 0)
+                            new(-size, -size, 0),
+                            new(-size, size, 0),
+                            new(size, size, 0),
+                            new(size, -size, 0)
                         };
 
-                        Quaternion particleRotation = Quaternion.Euler(particle.rotation3D);
+                        var particleRotation = Quaternion.Euler(particle.rotation3D);
 
-                        _quad[0].position = pos3d + particleRotation * verts[0];
-                        _quad[1].position = pos3d + particleRotation * verts[1];
-                        _quad[2].position = pos3d + particleRotation * verts[2];
-                        _quad[3].position = pos3d + particleRotation * verts[3];
+                        quad[0].position = pos3d + particleRotation * verts[0];
+                        quad[1].position = pos3d + particleRotation * verts[1];
+                        quad[2].position = pos3d + particleRotation * verts[2];
+                        quad[3].position = pos3d + particleRotation * verts[3];
                     }
                     else
                     {
-                        // apply rotation
-                        Vector2 right = new Vector2(Mathf.Cos(rotation), Mathf.Sin(rotation)) * size;
-                        Vector2 up = new Vector2(Mathf.Cos(rotation90), Mathf.Sin(rotation90)) * size;
+                        var right = new Vector2(Mathf.Cos(rotation), Mathf.Sin(rotation)) * size;
+                        var up = new Vector2(Mathf.Cos(rotation90), Mathf.Sin(rotation90)) * size;
 
-                        _quad[0].position = position - right - up;
-                        _quad[1].position = position - right + up;
-                        _quad[2].position = position + right + up;
-                        _quad[3].position = position + right - up;
+                        quad[0].position = position - right - up;
+                        quad[1].position = position - right + up;
+                        quad[2].position = position + right + up;
+                        quad[3].position = position + right - up;
                     }
                 }
 
-                vh.AddUIVertexQuad(_quad);
+                vh.AddUIVertexQuad(quad);
             }
         }
 
@@ -341,13 +286,13 @@ namespace UnityEngine.UI.Extensions.CasualGame
         {
             if (!fixedTime && Application.isPlaying)
             {
-                pSystem.Simulate(Time.unscaledDeltaTime, false, false, true);
+                ps.Simulate(Time.unscaledDeltaTime, false, false, true);
                 SetAllDirty();
 
                 if ((currentMaterial != null && currentTexture != currentMaterial.mainTexture) ||
                     (material != null && currentMaterial != null && material.shader != currentMaterial.shader))
                 {
-                    pSystem = null;
+                    ps = null;
                     Initialize();
                 }
             }
@@ -363,19 +308,22 @@ namespace UnityEngine.UI.Extensions.CasualGame
             {
                 if (fixedTime)
                 {
-                    pSystem.Simulate(Time.unscaledDeltaTime, false, false, true);
+                    ps.Simulate(Time.unscaledDeltaTime, false, false, true);
                     SetAllDirty();
                     if ((currentMaterial != null && currentTexture != currentMaterial.mainTexture) ||
                         (material != null && currentMaterial != null && material.shader != currentMaterial.shader))
                     {
-                        pSystem = null;
+                        ps = null;
                         Initialize();
                     }
                 }
             }
             if (material == currentMaterial)
+            {
                 return;
-            pSystem = null;
+            }
+
+            ps = null;
             Initialize();
         }
 
@@ -387,17 +335,17 @@ namespace UnityEngine.UI.Extensions.CasualGame
 
         public void StartParticleEmission()
         {
-            pSystem.Play();
+            ps.Play();
         }
 
         public void StopParticleEmission()
         {
-            pSystem.Stop(false, ParticleSystemStopBehavior.StopEmittingAndClear);
+            ps.Stop(false, ParticleSystemStopBehavior.StopEmittingAndClear);
         }
 
         public void PauseParticleEmission()
         {
-            pSystem.Stop(false, ParticleSystemStopBehavior.StopEmitting);
+            ps.Stop(false, ParticleSystemStopBehavior.StopEmitting);
         }
     }
 #endif
