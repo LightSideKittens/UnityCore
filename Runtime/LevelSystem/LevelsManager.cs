ec68abff875ec42c8fceec06cdbd98c03ee7ed54
Malvis Light <cat@lightside.games>
2024-09-29 23:49:02 +0400

    Improved LevelsManager❤️

--- Runtime/LevelSystem/LevelsManager.cs
+++ Runtime/LevelSystem/LevelsManager.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Sirenix.OdinInspector;
 using Sirenix.Serialization;
 using UnityEngine;
@@ -8,6 +9,9 @@ namespace LSCore.LevelSystem
 {
     public partial class LevelsManager : SerializedScriptableObject
     {
+        public event Action<Id, int> LevelChanged;
+        private Dictionary<Id, Action<int>> LevelChangedById { get; } = new();
+        
         [IdGroup]
         [OdinSerialize]
         [HideReferenceObjectPicker]
@@ -60,33 +64,60 @@ namespace LSCore.LevelSystem
             }
         }
 
-        public bool CanUpgrade(Id id)
+        public void SubAndCallLevelChanged(Id id, Action<int> action)
         {
-            UnlockedLevels.TryGetLevel(id, out var currentLevel);
-            return currentLevel < levelsById[id].Count;
+            LevelChangedById.TryGetValue(id, out var existAction);
+            existAction += action;
+            LevelChangedById[id] = existAction; 
+            action(GetCurrentLevelNum(id));
+        }
+        
+        public void UnSubLevelChanged(Id id, Action<int> action)
+        {
+            LevelChangedById.TryGetValue(id, out var existAction);
+            existAction -= action;
+            LevelChangedById[id] = existAction; 
+        }
+
+        private void OnSetLevel(Id id, int level)
+        {
+            UnlockedLevels.TryGetLevel(id, out var oldLevel);
+            if (level != oldLevel)
+            {
+                LevelChanged?.Invoke(id, level);
+                if (LevelChangedById.TryGetValue(id, out var action))
+                {
+                    action(level);
+                }
+            }
         }
 
         public void SetLevel(Id id, int level)
         {
             var levels = levelsById[id];
-            UnlockedLevels.SetLevel(id, 
-                level < levels.Count 
-                    ? level 
-                    : levels.Count);
+            
+            level = level < levels.Count
+                ? level
+                : levels.Count;
+
+            OnSetLevel(id, level);
+            UnlockedLevels.SetLevel(id, level);
         }
         
-        public void SetLevel(int level)
+        public void SetLevelForAll(int level)
         {
             foreach (var (id, levels) in levelsById)
             {
-                UnlockedLevels.SetLevel(id, 
-                    level < levels.Count 
-                        ? level 
-                        : levels.Count);
+                var newLevel = level < levels.Count
+                    ? level
+                    : levels.Count;
+                
+                OnSetLevel(id, level);
+                UnlockedLevels.SetLevel(id, newLevel);
             }
         }
 
-        public int UpgradeLevel(Id id)
+        public void UpgradeLevel(Id id)
         {
             UnlockedLevels.TryGetLevel(id, out var currentLevel);
             var levels = levelsById[id];
@@ -94,12 +125,10 @@ namespace LSCore.LevelSystem
             if (currentLevel < levels.Count)
             {
                 currentLevel++;
+                OnSetLevel(id, currentLevel);
                 UnlockedLevels.SetLevel(id, currentLevel);
                 Burger.Log($"{id} Upgraded to {currentLevel}");
             }
-
-            return currentLevel;
-            
         }
 
         public T GetCurrentLevel<T>(Id id) where T : Object
@@ -108,6 +137,22 @@ namespace LSCore.LevelSystem
             return GetLevel<T>(id, level);
         }
         
+        public int GetCurrentLevelNum(Id id)
+        {
+            UnlockedLevels.TryGetLevel(id, out var level);
+            return level;
+        }
+
+        public int GetMaxLevel(Id id) => levelsById[id].Count;
+        
+        public bool IsMaxLevel(Id id)
+        {
+            UnlockedLevels.TryGetLevel(id, out var level);
+            return level >= levelsById[id].Count;
+        }
+        
+        public bool IsUnlocked(Id id) => UnlockedLevels.TryGetLevel(id, out var level) && level > 0;
+
         public T GetLevel<T>(Id id, int level) where T : Object
         {
 #if UNITY_EDITOR
