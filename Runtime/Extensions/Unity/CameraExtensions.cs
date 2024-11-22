cb4429739c13fa68c9eca3075a9df6873b6c1cba
Malvis Light <cat@lightside.games>
2024-11-22 14:40:25 +0400

    Raw Tooltip implementation❤️

--- Runtime/Extensions/Unity/CameraExtensions.cs
+++ Runtime/Extensions/Unity/CameraExtensions.cs
@@ -19,9 +19,45 @@ namespace LSCore.Extensions.Unity
         
         public static Rect GetRect(this Camera camera)
         {
-            var rect = new Rect(camera.transform.position, camera.GetSize());
-            rect.center = camera.transform.position;
+            var pos = camera.transform.position;
+            var rect = new Rect(pos, camera.GetSize());
+            rect.center = pos;
             return rect;
+        }
+        
+        public static Vector2 GetSize(this Camera camera, float distance)
+        {
+           float halfHeight = Mathf.Tan(camera.fieldOfView * 0.5f * Mathf.Deg2Rad) * distance;
+           float halfWidth = halfHeight * camera.aspect;
+           return new Vector2(halfWidth * 2, halfHeight * 2);
+        }
+        
+        public static Rect GetRect(this Camera camera, float distance)
+        {
+            var pos = camera.transform.position;
+            var rect = new Rect(pos, camera.GetSize(distance));
+            rect.center = pos;
+            return rect;
+        }
+        
+        public static Plane PlaneAt(this Camera camera, float distance)
+        {
+            var tr = camera.transform;
+            var forward = tr.forward;
+            Vector3 pointOnPlane = tr.position + forward * distance;
+            Plane plane = new Plane(forward, pointOnPlane);
+            
+            return plane;
+        }
+        
+        public static Plane PlaneAt(this Canvas canvas)
+        {
+            var tr = canvas.transform;
+            var forward = tr.forward;
+            Vector3 pointOnPlane = tr.position;
+            Plane plane = new Plane(forward, pointOnPlane);
+            
+            return plane;
         }
     }
 }
--- Runtime/UIModule/Components/Tooltip.cs
+++ Runtime/UIModule/Components/Tooltip.cs
@@ -1,5 +1,7 @@
-﻿using LSCore.Extensions.Unity;
+﻿using System;
+using LSCore.Extensions.Unity;
 using Sirenix.OdinInspector;
+using Sirenix.Utilities;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -11,38 +13,122 @@ namespace LSCore
         public LSText tooltipText;
         public RectTransform pointer;
         public Camera mainCamera;
+        public Canvas canvas;
         public float defaultPointOffset;
+        public float defaultTooltipContainerOffset;
+        private Direction direction;
+
+        [Button]
+        public void ShowTooltip(Transform worldPoint, string message)
+        {
+            ShowTooltip(worldPoint.position, message);
+        }
+        
 
         [Button]
         public void ShowTooltip(Vector3 worldPoint, string message)
         {
             tooltipText.text = message;
             LayoutRebuilder.ForceRebuildLayoutImmediate(tooltipContainer);
-            Rect cameraBounds = mainCamera.GetRect();
+            
+            Rect cameraBounds = GetRect();
             tooltipContainer.gameObject.SetActive(true);
-            Vector2 pos = mainCamera.transform.InverseTransformPoint(GetProjectionPointOnCameraPlane(worldPoint));
-            
-            if (pos.y >= 0)
+            var plane = mainCamera.PlaneAt(canvas.planeDistance);
+            var camPos = mainCamera.transform.position;
+            var forward = mainCamera.transform.forward;
+            var dir = worldPoint - camPos;
+            var dot = Vector3.Dot(forward, dir);
+
+            if (dot < 0)
             {
-                
-            }
-            else if (pos.y < 0)
-            {
-                
-            }
-            else if (pos.x > 0)
-            {
-                
-            }
-            else
-            {
-                
+                var forwardComponent = dot * forward;
+                dir -= 2 * forwardComponent;
             }
             
-            Vector2 tooltipPosition = GetBestTooltipPosition(cameraBounds, worldPoint);
-            tooltipContainer.position = tooltipPosition;
+            var ray = new Ray(camPos, dir);
+            plane.Raycast(ray, out float e);
+            dir = dir.normalized;
             
-            PositionPointer(cameraBounds, tooltipPosition, 1);
+            Vector3 pos = mainCamera.transform.InverseTransformPoint(camPos + dir * e);
+            var side = DetermineQuadrant(pos, cameraBounds.width, cameraBounds.height);
+            pos = mainCamera.transform.TransformPoint(pos);
+            
+            var sideDir = GetDir(side);
+
+            pos -= sideDir * defaultTooltipContainerOffset;
+            tooltipContainer.pivot = GetPivot(side);
+            tooltipContainer.position = pos;
+
+            pos -= sideDir * defaultPointOffset;
+            pointer.localEulerAngles = new Vector3(0, 0, GetRot(side));
+            pointer.position = pos;
+        }
+
+        private Rect GetRect()
+        {
+            if (mainCamera.orthographic)
+            {
+                return mainCamera.GetRect();
+            }
+            
+            return mainCamera.GetRect(canvas.planeDistance);
+        }
+        
+        private Vector3 GetDir(Direction direction)
+        {
+            var tr = mainCamera.transform;
+            return direction switch
+            {
+                Direction.Left => -tr.right,
+                Direction.Right => tr.right,
+                Direction.Top => tr.up,
+                Direction.Bottom => -tr.up
+            };
+        }
+        
+        private static Vector2 GetPivot(Direction direction)
+        {
+            return direction switch
+            {
+                Direction.Left => new Vector2(0, 0.5f),
+                Direction.Right => new Vector2(1, 0.5f),
+                Direction.Top => new Vector2(0.5f, 1f),
+                Direction.Bottom => new Vector2(0.5f, 0f),
+            };
+        }
+        
+        private static float GetRot(Direction direction)
+        {
+            return direction switch
+            {
+                Direction.Left => 90,
+                Direction.Right => -90,
+                Direction.Top => 0,
+                Direction.Bottom => 180
+            };
+        }
+
+        private static Direction DetermineQuadrant(Vector2 localPoint, float cameraWidth, float cameraHeight)
+        {
+            var xNorm = localPoint.x / (cameraWidth / 2);
+            var yNorm = localPoint.y / (cameraHeight / 2);
+            
+            if (yNorm > xNorm && yNorm > -xNorm)
+            {
+                return Direction.Top;
+            }
+
+            if (yNorm > xNorm && yNorm < -xNorm)
+            {
+                return Direction.Left;
+            }
+            
+            if (yNorm < xNorm && yNorm > -xNorm)
+            {
+                return Direction.Right;
+            }
+            
+            return Direction.Bottom;
         }
         
         private Vector3 GetProjectionPointOnCameraPlane(Vector3 objectPosition)
