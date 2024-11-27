f38c957a84b8a49a16acb56256ebc7ba790c0329
Yulia Light <kitty@lightside.games>
2024-11-27 19:06:31 +0400

    Add FilledAmountAnim🐵

---
+++ Runtime/AnimationsModule/Animations/FilledAmountAnim.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using DG.Tweening;
+using UnityEngine.UI;
+
+namespace LSCore.AnimationsModule.Animations
+{
+    [Serializable]
+    public class FilledAmountAnim : BaseAnim<float, Image>
+    {
+        protected override void InitAction(Image target)
+        {
+            target.fillAmount = startValue;
+        }
+
+        protected override Tween AnimAction(Image target)
+        {
+            return target.DOFillAmount(endValue, Duration);
+        }
+    }
+}
