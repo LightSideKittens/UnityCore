67d26c4297f2c9051e93ac661ce879c0eda4e628
Malvis Light <cat@lightside.media>
2025-03-27 18:39:22 +0400

    Improved Wait.cs❤️

--- Runtime/Async/Wait.cs
+++ Runtime/Async/Wait.cs
@@ -9,6 +9,7 @@ namespace LSCore.Async
         public static Tween Run(in float time, TweenCallback<float> update) => DOVirtual.Float(0, 1, time, update).SetEase(Ease.Linear);
         public static Tween InverseRun(in float time, TweenCallback<float> update) => DOVirtual.Float(1, 0, time, update).SetEase(Ease.Linear);
         public static Tween Run(in float time, TweenCallback update) =>  DOTween.Sequence().AppendInterval(time).OnUpdate(update).SetEase(Ease.Linear);
+        public static Tween FromTo(float from, float to, float time, TweenCallback<float> update) =>  DOVirtual.Float(from, to, time, update).SetEase(Ease.Linear);
         public static Tween TimerForward(in float time, TweenCallback<float> update) => DOVirtual.Float(0, time, time, update).SetEase(Ease.Linear);
         public static Tween TimerBack(in float time, TweenCallback<float> update) => DOVirtual.Float(time, 0, time, update).SetEase(Ease.Linear);
         public static Tween Delay(in float time, TweenCallback onComplete) => Delay(time).OnComplete(onComplete);
