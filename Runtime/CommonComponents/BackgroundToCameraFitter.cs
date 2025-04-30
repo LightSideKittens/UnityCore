214ae82d41dfc5075fa524b2e65b9adf2fe0c6e5
Malvis Light <cat@lightside.media>
2025-04-30 15:34:39 +0400

    Added some CommonComponents❤️

---
+++ Runtime/CommonComponents/BackgroundToCameraFitter.cs
@@ -0,0 +1,52 @@
+﻿using LSCore.Extensions.Unity;
+using UnityEngine;
+
+namespace LSCore
+{
+    [RequireComponent(typeof(Camera))]
+    [ExecuteAlways]
+    public class BackgroundToCameraFitter : MonoBehaviour
+    {
+        public SpriteRenderer backgroundRenderer;
+        private Camera cam;
+
+        private void Awake()
+        {
+            cam = GetComponent<Camera>();
+            if (!cam.orthographic)
+            {
+                Burger.Warning($"{nameof(BackgroundToCameraFitter)}: enable Orthographic on camera");
+                cam.orthographic = true;
+            }
+        }
+
+        private void Start() => Fit();
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if(cam) Fit();
+        }
+        private void Update() => Fit();
+#else
+        void OnEnable() => Fit();
+#endif
+
+        private void Fit()
+        {
+            if (!backgroundRenderer) return;
+            
+            Vector2 spriteSize = backgroundRenderer.sprite.bounds.size;
+            var camSize = cam.GetSize();
+            
+            float k = Mathf.Max(camSize.x / spriteSize.x, camSize.y / spriteSize.y);
+            var newScale = Vector3.one * k;
+            
+            backgroundRenderer.transform.localScale = newScale;
+            
+            Vector3 pos = cam.transform.position;
+            var backPos = backgroundRenderer.transform.position; 
+            pos.z = backPos.z;
+            backgroundRenderer.transform.position = pos;
+        }
+    }
+}
---
+++ Runtime/CommonComponents/CameraToBackgroundFitter.cs
@@ -0,0 +1,65 @@
+﻿using LSCore.Extensions.Unity;
+using UnityEngine;
+
+namespace LSCore
+{
+    [RequireComponent(typeof(Camera))]
+    [ExecuteAlways]
+    public class CameraToBackgroundFitter : MonoBehaviour
+    {
+        public SpriteRenderer backgroundRenderer;
+        public float cameraSizeRef = 10;
+        private Camera cam;
+
+        private void Awake()
+        {
+            cam = GetComponent<Camera>();
+            if (!cam.orthographic)
+            {
+                Burger.Warning($"{nameof(CameraToBackgroundFitter)}: enable Orthographic on camera");
+                cam.orthographic = true;
+            }
+        }
+
+        private void Start() => Fit();
+#if UNITY_EDITOR
+        private void OnValidate()
+        {
+            if(cam) Fit();
+        }
+
+        private void Update() => Fit();
+#else
+        void OnEnable() => Fit();
+#endif
+
+        private void Fit()
+        {
+            if (!backgroundRenderer) return;
+
+            Bounds b = backgroundRenderer.bounds;
+            float w = b.size.x;
+            float h = b.size.y;
+            
+            var camSize = CameraExtensions.GetSize(cameraSizeRef, cam.aspect);
+
+            if (camSize.x < w)
+            {
+                cam.SetSizeByWidth(w);
+            }
+            else if(camSize.y < h)
+            {
+                cam.SetSizeByHeight(h);
+            }
+            else
+            {
+                cam.orthographicSize = cameraSizeRef;
+            }
+            
+            Vector3 pos = cam.transform.position;
+            pos.x = b.center.x;
+            pos.y = b.center.y;
+            cam.transform.position = pos;
+        }
+    }
+}
