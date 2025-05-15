378d6d62958d8c25892a3e7f0cf69c3602447455
Malvis Light <cat@lightside.media>
2025-05-15 06:19:12 +0400

    Implemented LSEventSystem❤️

---
+++ Runtime/LSDelegates/LSEventSystem.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class LSInvoker : LSAction
+{
+    public string key;
+    
+    public override void Invoke()
+    {
+        StringDict<Action>.Get(key)?.Invoke();
+    }
+}
+
+public class LSSubscriber : LSAction
+{
+    public string key;
+    public List<LSAction> actions;
+    
+    public override void Invoke()
+    {
+        var action = StringDict<Action>.Get(key);
+        action += actions.Invoke;
+        StringDict<Action>.Set(key, action);
+    }
+}
