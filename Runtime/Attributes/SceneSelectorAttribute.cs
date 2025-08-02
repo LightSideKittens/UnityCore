d8955a61cf64bedc99a9d96df01c67961b387580
Malvis Light <cat@lightside.media>
2025-08-02 21:09:41 +0400

    Improved SingleAssets❤️

---
+++ Runtime/Attributes/SceneSelectorAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+using System.Diagnostics;
+using Sirenix.OdinInspector;
+using UnityEngine.SceneManagement;
+
+namespace LSCore.Attributes
+{
+    [Conditional("UNITY_EDITOR")]
+    public class SceneSelectorAttribute : ValueDropdownAttribute
+    {
+        public SceneSelectorAttribute() : base("") { }
+        
+        public static IEnumerable<string> SceneNames
+        {
+            get
+            {
+                int sceneCount = SceneManager.sceneCountInBuildSettings;
+
+                for (int i = 0; i < sceneCount; i++)
+                {
+                    string scenePath = SceneUtility.GetScenePathByBuildIndex(i);
+                    string sceneName = System.IO.Path.GetFileNameWithoutExtension(scenePath);
+                    yield return sceneName;
+                }
+            }
+        }
+    }
+}
--- Runtime/Extensions/LSMath.cs
+++ Runtime/Extensions/LSMath.cs
@@ -1,8 +1,15 @@
-﻿public static class LSMath
+﻿using System.Collections.Generic;
+
+public static class LSMath
 {
     public static int WrapIndex(int value, int min, int max)
     {
         if(value < min) return value;
         return (value - min) % (max - min) + min;
+    }
+
+    public static T GetWrapped<T>(this IList<T> list, int value, int min)
+    {
+        return list[WrapIndex(value, min, list.Count)];
     }
 }
