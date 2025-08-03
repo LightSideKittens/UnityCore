e8511b9907f1c1ff795194c062910aa4c7fb8148
Malvis Light <cat@lightside.media>
2025-08-03 22:06:16 +0400

    Fixed some bugs and errors❤️

--- Runtime/ScriptableObjects/SingleAssets.cs
+++ Runtime/ScriptableObjects/SingleAssets.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using LSCore;
 using LSCore.DataStructs;
 using Sirenix.OdinInspector;
 using UnityEngine;
@@ -11,19 +12,49 @@ public class FromSingleAssetsAttribute : ValueDropdownAttribute
     {
         
     }
-} 
+}
 
 public class SingleAssets : ScriptableObject
 {
     public UniDict<string, Object> assets;
     [OnValueChanged("OnTypeAssetsChanged")] public List<Object> typedAssets;
     private static SingleAssets instance;
-    private Dictionary<Type, Object> typedAssetsDict;
-    private static SingleAssets Instance => instance ?? Resources.Load<SingleAssets>("SingleAssets");
-    
+    private Dictionary<Type, Object> typedAssetsDict = new();
+    private static SingleAssets Instance
+    {
+        get
+        {
+            if (instance == null)
+            {
+                instance = Resources.Load<SingleAssets>("SingleAssets");
+                instance.Init();
+            }
+
+            return instance;
+        }
+    }
+
     public static IEnumerable<string> Keys => Instance.assets.Keys;
     public static T Get<T>(string key) where T : Object => (T)Instance.assets[key];
     public static T Get<T>() where T : Object => (T)Instance.typedAssetsDict[typeof(T)];
+
+    private void Init()
+    {
+        typedAssetsDict.Clear();
+        for (var i = 0; i < typedAssets.Count; i++)
+        {
+            typedAssetsDict.Add(typedAssets[i].GetType(), typedAssets[i]);
+        }
+
+        World.Destroyed += OnDestroy;
+
+        void OnDestroy()
+        {
+            World.Destroyed -= OnDestroy;
+            Resources.UnloadAsset(this);
+            instance = null;
+        }
+    }
     
     private void OnTypeAssetsChanged()
     {
