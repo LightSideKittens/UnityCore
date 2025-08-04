31a8b944c20deeec642a027287254fa3fc948aee
Malvis Light <cat@lightside.media>
2025-08-04 04:31:27 +0400

    Little TransformExtension improvements❤️

--- Runtime/Extensions/Unity/TransformExtensions.cs
+++ Runtime/Extensions/Unity/TransformExtensions.cs
@@ -26,6 +26,28 @@ namespace LSCore.Extensions.Unity
             transform.position = pos;
         }
         
+        public static void LocalX(this Transform transform, float x)
+        {
+            var pos = transform.localPosition;
+            pos.x = x;
+            transform.localPosition = pos;
+        }
+        
+        public static void LocalY(this Transform transform, float y)
+        {
+            var pos = transform.localPosition;
+            pos.y = y;
+            transform.localPosition = pos;
+        }
+        
+        public static void LocalXY(this Transform transform, float x, float y)
+        {
+            var pos = transform.localPosition;
+            pos.x = x;
+            pos.y = y;
+            transform.localPosition = pos;
+        }
+        
         public static void SetLossyScale(this Transform tr, Vector3 worldScale)
         {
             Vector3 parentScale = tr.parent ? tr.parent.lossyScale : Vector3.one;
