22447809664c37eb29d973e95223dfa820d786bb
Malvis Light <cat@lightside.media>
2025-08-28 17:49:13 +0400

    Little AnimationModule improves❤️

---
+++ Runtime/AnimationsModule/Animations/Delay.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using DG.Tweening;
+using LSCore.Async;
+
+namespace LSCore.AnimationsModule.Animations
+{
+    [Serializable]
+    public class Delay : BaseAnim
+    {
+        public float delay;
+        public override bool NeedInit => false;
+        protected override void Internal_Init() { }
+        
+        protected override Tween Internal_Animate()
+        {
+            return Wait.Delay(delay);
+        }
+    }
+}
