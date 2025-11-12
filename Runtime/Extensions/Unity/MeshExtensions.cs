8b722825ce818c6e228df4467120adba4c380ccf
Malvis Light <cat@lightside.media>
2025-11-12 05:17:30 +0400

    Little fixes❤️

---
+++ Runtime/Extensions/Unity/MeshExtensions.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+public static class MeshExtensions
+{
+    public static void Scale(this Mesh mesh, Vector3 scale, Vector3 pivot)
+    {
+        var verts = mesh.vertices;
+        for (int i = 0; i < verts.Length; i++)
+        {
+            var v = verts[i] - pivot;
+            v = Vector3.Scale(v, scale);
+            verts[i] = v + pivot;
+        }
+        
+        mesh.vertices = verts;
+    }
+}
