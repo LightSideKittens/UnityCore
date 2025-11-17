67156e683a4fe73ee7b469276e50dccb1fc16a43
Malvis Light <cat@lightside.media>
2025-11-17 10:42:52 +0400

    Improved JTokenExtensions❤️

--- Runtime/Extensions/JTokenExtensions_Reactive.cs
+++ Runtime/Extensions/JTokenExtensions_Reactive.cs
@@ -11,8 +11,41 @@ namespace LSCore.Extensions
         private static Dictionary<JContainer, Dictionary<Action, Action>> unlistens = new();
         
         public static void ListenAndCall(this JToken token, Action action) => token.Parent.ListenAndCall(action);
-        public static void UnListen(this JToken token, Action action) => token.Parent.UnListen(action);
 
+        public static void UnListen(this JToken token, Action action) => token.Parent.UnListen(action);
+        
+        public static void ListenAndCall(this JObject obj, string key, Action action)
+        {
+            if (!unlistens.TryGetValue(obj, out var unlistenDict))
+            {
+                unlistenDict = new();
+                unlistens.Add(obj, unlistenDict);
+            }
+            
+            unlistenDict.TryGetValue(action, out var unlisten);
+            unlisten += UnListen;
+            unlistenDict[action] = unlisten;
+            
+            obj.PropertyChanged += OnPropertyChanged;
+            action();
+            
+            void OnPropertyChanged(object sender, PropertyChangedEventArgs e)
+            {
+                if (key == e.PropertyName)
+                {
+                    action();
+                }
+            }
+
+            void UnListen()
+            {
+                obj.PropertyChanged -= OnPropertyChanged;
+                unlistenDict.TryGetValue(action, out var nunlisten);
+                nunlisten -= UnListen;
+                unlistenDict[action] = nunlisten;
+            }
+        }
+        
         public static void ListenAndCall(this JContainer token, Action action)
         {
             if (!unlistens.TryGetValue(token, out var unlistenDict))
